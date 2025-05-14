497de83283d02ad14c1a6f9a86cf0af50ee8b946
naimur2001 <naimurrahman18thapril@gmail.com>
2025-05-14 18:44:54 +0600

    initial

---
+++ index.js
@@ -0,0 +1,10 @@
+"use strict";
+console.log("Ami TS");
+const number = 10;
+console.log(number);
+function abcd() {
+    const x = 20;
+    const y = 60;
+    console.log(x + y);
+}
+abcd();
---
+++ index.ts
@@ -0,0 +1,12 @@
+console.log("Ami TS");
+const number = 10;
+console.log(number);
+
+
+function abcd(){
+   const x=20
+   const y=60
+   console.log(x+y)
+}
+
+abcd()
